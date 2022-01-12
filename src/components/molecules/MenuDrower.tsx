b79ad2eb1b9acf7df8f8a7b25a693d395f77a036
RyosukeSomeya <rx.0.0618@gmail.com>
2022-01-13 01:37:45 +0900

    ヘッダーにルーティング機能実装

--- src/components/molecules/MenuDrower.tsx
+++ src/components/molecules/MenuDrower.tsx
@@ -9,19 +9,29 @@ import { memo, VFC } from 'react';
 
 type Props = {
   onClose: () => void;
+  onClickHome: () => void;
+  onClickUserManagement: () => void;
+  onClickSetting: () => void;
   isOpen: boolean;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { onClose, isOpen } = props;
+  const { onClose, isOpen, onClickHome, onClickUserManagement, onClickSetting } =
+    props;
   return (
     <Drawer placement='left' size='xs' onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg='gray.100'>
-            <Button w='100%'>TOP</Button>
-            <Button w='100%'>ユーザー一覧</Button>
-            <Button w='100%'>設定</Button>
+            <Button w='100%' onClick={onClickHome}>
+              TOP
+            </Button>
+            <Button w='100%' onClick={onClickUserManagement}>
+              ユーザー一覧
+            </Button>
+            <Button w='100%' onClick={onClickSetting}>
+              設定
+            </Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
